ceb7ebc0bc9dcf18f05c27d7999b0b1be74ea789
prabin123-proo <71684870+prabin123-proo@users.noreply.github.com>
2020-10-01 21:30:00 +0545

    impactful update
    this make your programme more intresting to viewers or other programmers :) hope you like it

--- Hello-world.py
+++ Hello-world.py
@@ -1,8 +1,10 @@
 #! /usr/bin/python3
 
 def main():
-    print("Hello-world!")
-    print ("This is my first branch")
+    
+    user_name =input("Please enter name") 
+    print(f"Hello-world! {user_name}")
+    print (f"{user_name}, This is your first branch :)")
 
 if __name__ == "__main__":
     main()
